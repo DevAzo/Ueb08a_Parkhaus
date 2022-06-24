21c75a0bc01344301ec3e9c0f8ceb7e1e0b01ae7
Kurt Müllner <kurt.muellner@bluewin.ch>
2022-06-24 16:19:51 +0200

    Testcase erweitert.

--- Ueb08a_ParkhausTest/TestAuto.cs
+++ Ueb08a_ParkhausTest/TestAuto.cs
@@ -9,12 +9,14 @@ namespace Ueb08a_ParkhausTest
         public void Test_Auto_Konstruktor()
         {
             var auto = new Auto();
+            Assert.IsTrue(auto != null, "kein Speicher vorhanden");
         }
 
         [TestMethod]
         public void Test_Auto_Konstruktor_mit_Werten()
         {
             var auto = new Auto() { Kennzeichen = 607609, Name="Opel"};
+            Assert.IsTrue(auto.Kennzeichen == 123456);
         }
     }
 }
